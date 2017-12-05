30d7706c8b413da217ad61d9bf8009899b1974fc
Estela Parrado <estela.parrado@gmail.com>
2017-12-05 12:56:30 +0100

    js for cv and form nav buttons

--- js/main.js
+++ js/main.js
@@ -20,3 +20,35 @@ for (var i = startYear; i <= currentYear; i++) {
 }
 
 document.getElementById('startExpYear').innerHTML = options;*/
+
+/*Funcion para cambiar entre FORM y CV al clicar en las pestañas-nav*/
+var switchFormButton = document.querySelector('.tab-form-button');
+var switchCvButton = document.querySelector('.tab-cv-button');
+var showForm = document.querySelector('.form-box');
+var showCv = document.querySelector('.cv-box');
+
+function switchCv () {
+  showCv.classList.remove('hidden') + showForm.classList.add('hidden') + switchCvButton.classList.add('active') + switchFormButton.classList.remove('active');
+}
+switchCvButton.addEventListener('click', switchCv);
+
+function switchForm () {
+  showCv.classList.add('hidden') + showForm.classList.remove('hidden') + switchFormButton.classList.add('active') + switchCvButton.classList.remove('active');;
+}
+switchFormButton.addEventListener('click',switchForm)
+
+
+/*Funcion para desplegar fieldeset al clicar en editar*/
+var editButton = document.querySelector('.edit-button');
+var showContent = document.querySelector('.contact-input');
+function edit () {
+  showContent.classList.remove('hidden');
+}
+editButton.addEventListener('click', edit);
+
+/*Funcion para cerrar fieldeset al clicar en cerrar*/
+var closeButton = document.querySelector('.close-input-button')
+function close () {
+  showContent.classList.add('hidden');
+}
+closeButton.addEventListener('click', close);
