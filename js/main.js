e0d7a6ee3af9de86aae96f9154c97b4d56b30479
Estela Parrado <estela.parrado@gmail.com>
2017-12-06 14:14:42 +0100

    colapsables fieldset working ok

--- js/main.js
+++ js/main.js
@@ -1,54 +1,63 @@
-'use strict'
-
-function addYears(initYear, currentYear) {
-  var acumulador = '';
-  for (var i = initYear; i <= currentYear; i++) {
-    acumulador = acumulador + '<option>' + i + '</option>';
-  }
-  return '<option id="startExpMonth" disabled selected>Año</option>' + acumulador;
-}
-
-var selectYears = document.querySelector('startExpYear');
-
-startExpYear.innerHTML = addYears(1950, 2017);
-
-/*var startYear = 1950;
-var currentYear = 2017;
-var options = '';
-for (var i = startYear; i <= currentYear; i++) {
-  options += '<option>' + i + '</option>'
-}
-
-document.getElementById('startExpYear').innerHTML = options;*/
+'use strict';
 
 /*Funcion para cambiar entre FORM y CV al clicar en las pestañas-nav*/
-var switchFormButton = document.querySelector('.tab-form-button');
-var switchCvButton = document.querySelector('.tab-cv-button');
+var tabButton = document.querySelectorAll('.tab-button');
 var showForm = document.querySelector('.form-box');
 var showCv = document.querySelector('.cv-box');
 
-function switchCv () {
-  showCv.classList.remove('hidden') + showForm.classList.add('hidden') + switchCvButton.classList.add('active') + switchFormButton.classList.remove('active');
+for (var i = 0; i < tabButton.length; i++) {
+  tabButton[i].addEventListener('click', switchCv);
 }
-switchCvButton.addEventListener('click', switchCv);
 
-function switchForm () {
-  showCv.classList.add('hidden') + showForm.classList.remove('hidden') + switchFormButton.classList.add('active') + switchCvButton.classList.remove('active');;
+function switchCv () {
+  showCv.classList.toggle('hidden');
+  showForm.classList.toggle('hidden');
+  for (var i = 0; i < tabButton.length; i++) {
+    tabButton[i].classList.toggle('active');
+  }
 }
-switchFormButton.addEventListener('click',switchForm)
 
 
 /*Funcion para desplegar fieldeset al clicar en editar*/
-var editButton = document.querySelector('.edit-button');
-var showContent = document.querySelector('.contact-input');
-function edit () {
+
+function showContent(event) {
+  var content = document.querySelectorAll('.content');
+  var idButton = event.currentTarget.getAttribute('data-id');
+  content[idButton].classList.toggle('hidden');
+}
+
+var editButton = document.querySelectorAll('.edit-button');
+for (var i = 0; i < editButton.length; i++) {
+  editButton[i].addEventListener('click', showContent);
+};
+
+
+/*Funcion para cerrar los fieldset al clicar en cerrar*/
+/*function closeContent(event) {
+  var content = document.querySelectorAll('.content');
+  var closeIdButton = event.currentTarget.getAttribute('data-id');
+  content[closeIdButton].classList.add('hidden');
+}
+
+var closeButton = document.querySelectorAll('.close-button-input');
+for (var i = 0; i < closeButton.length; i++) {
+  closeButton[i].addEventListener('click', closeContent);
+};*/
+
+/*var editButton = document.querySelectorAll('.edit-button');
+var showContent = document.querySelector('.content');
+
+for (var i = 0; i < editButton.length; i++) {
+editButton[i].addEventListener('click', editContent);
+}
+
+function editContent (event) {
+  var progreso = event.currentTarget;
+  var input = progreso.
+  for (var i = 0; i < editButton.length; i++) {
   showContent.classList.remove('hidden');
 }
-editButton.addEventListener('click', edit);
-
-/*Funcion para cerrar fieldeset al clicar en cerrar*/
-var closeButton = document.querySelector('.close-input-button')
-function close () {
-  showContent.classList.add('hidden');
+for (var i = 0; i < showContent.length; i++) {
+  showContent[i].classList.add('hidden');
 }
-closeButton.addEventListener('click', close);
+};*/
