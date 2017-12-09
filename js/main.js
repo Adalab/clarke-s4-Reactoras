8c8eb125f8586bccb3ef4801586972fb2f94dbaf
Katirina Dimitrova <katirina79@gmail.com>
2017-12-09 18:25:11 +0100

    month changed

--- js/main.js
+++ js/main.js
@@ -33,23 +33,15 @@ for (var i = 0; i < editButton.length; i++) {
 // Desplegables mes
 
 var selectMonths = '<option selected disabled value="mes">Mes</option>';
-selectMonths = selectMonths + '<option value="enero">Enero</option>';
-selectMonths = selectMonths + '<option value="febrero">Febrero</option>';
-selectMonths = selectMonths + '<option value="marzo">Marzo</option>';
-selectMonths = selectMonths + '<option value="abril">Abril</option>';
-selectMonths = selectMonths + '<option value="mayo">Mayo</option>';
-selectMonths = selectMonths + '<option value="junio">Junio</option>';
-selectMonths = selectMonths + '<option value="julio">Julio</option>';
-selectMonths = selectMonths + '<option value="agosto">Agosto</option>';
-selectMonths = selectMonths + '<option value="septiembre">Septiembre</option>';
-selectMonths = selectMonths + '<option value="octubre">Octubre</option>';
-selectMonths = selectMonths + '<option value="noviembre">Noviembre</option>';
-selectMonths = selectMonths + '<option value="diciembre">Diciembre</option>';
+var monthArray = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 
-var monthsAll = document.querySelectorAll('.month');
-for (var i = 0; i < monthsAll.length;i++) {
-    monthsAll[i].innerHTML = selectMonths;
+for(i in monthArray){
+  selectMonths = selectMonths + '<option>' + (monthArray[i]) + '</option>';
 }
+var monthsAll = document.querySelectorAll('.month');
+for (var j = 0; j < monthsAll.length;j++) {
+    monthsAll[j].innerHTML = selectMonths;
+};
 
 // Desplegables año
 
@@ -58,8 +50,8 @@ var selectYears = '<option selected disabled value="año">Año</option>';
 
 for (var initYear = 1950; initYear < years; initYear++) {
   selectYears = selectYears + '<option>' + (initYear) + '</option>';
-}
+};
 var yearsAll = document.querySelectorAll('.year');
 for (var i = 0; i < yearsAll.length;i++) {
     yearsAll[i].innerHTML = selectYears;
-}
+};
