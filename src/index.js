dd50f6e985102fb8079208a71b28d7dd3d42e573
Elena <elenacerezo83@gmail.com>
2018-01-24 21:04:35 +0100

    Fix stylesheets path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/master.css';
-import './stylesheets/index.css';
+import './index.css';
 import App from './App';
 import main from './main';
 
