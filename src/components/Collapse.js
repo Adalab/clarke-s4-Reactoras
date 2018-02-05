ea58c9f21b40dba8ba1dceaa874339038f6bcdb3
“Ana” <amartinezaguilar6@gmail.com>
2018-02-05 17:21:19 +0100

    Add animation to fielsets

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -17,13 +17,13 @@ class Collapse extends Component {
 
   }
   render() {
-    const hiddenClass = this.state.collapsed ? 'hidden' : '';
+    const hiddenClass = this.state.collapsed ? 'hidden-box-form' : 'show-box-form';
 
     return (
       <fieldset className="default-fieldset">
           <legend>{this.props.legend}</legend>
           <button onClick={this.handleClickCollapse} className="edit-button squared-primaryColor-width100-button colored1-button-typo padding-xxxxs margin-xxxxs" type="button" name="button">Editar</button>
-          <div className={`contact-input content ${hiddenClass}`}>
+          <div className={` content ${hiddenClass}`}>
               {this.props.children}
               <button  onClick={this.handleClickCollapse} className="edit-button squared-primaryColor-width45-button left colored1-button-typo margin-md padding-xxxxs" type="button" name="button">cerrar</button>
 
